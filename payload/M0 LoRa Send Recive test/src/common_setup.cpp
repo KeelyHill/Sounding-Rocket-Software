50c396de125ed08efdec7cdcd14c9a89d24bfcb5
Keely Hill <KeelyHill@gmx.com>
2018-01-21 10:27:30 -0500

    Updated git ignore. Created a common setup header in payload directory.

--- payload/M0 LoRa Send Recive test/src/common_setup.cpp
+++
@@ -1,5 +0,0 @@
-
-
-void common_radio_setup() {
-
-}
---
+++ payload/common_setup.hpp
@@ -0,0 +1,13 @@
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#define RF95_FREQ 915.0
+
+void common_radio_setup() {
+
+}
